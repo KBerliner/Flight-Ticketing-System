b8b91035e1ca7ad8cf7c171ef07294cd622ebc8a
Kyle <berlinerkyle@gmail.com>
2026-02-20 11:07:23 +0100

    Added integration tests with the user object being stored in the new in-memory database.

---
+++ src/test/java/com/fts/flight_ticketing_system/integration/User_DatabaseTests.java
@@ -0,0 +1,26 @@
+package com.fts.flight_ticketing_system.integration;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.zip.DataFormatException;
+
+import org.junit.jupiter.api.Test;
+
+import com.fts.flight_ticketing_system.database.Database;
+import com.fts.flight_ticketing_system.database.Table;
+import com.fts.flight_ticketing_system.user.User;
+
+public class User_DatabaseTests {
+    @Test
+    void userTableShouldBeCreatedWithUserClass() throws DataFormatException {
+        Database db = new Database("Ticketing System");
+
+        Table userTable = db.createTable("User");
+
+        User newUser = new User("Username", "First", "Last", "email@gmail.com", "password");
+        
+        userTable.insertEntry("1234p97g", newUser.getUserAsHashMap());
+
+        assertNotNull(userTable.readEntry("1234p97g"));
+    }
+}
