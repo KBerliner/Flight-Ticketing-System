9963a011cccf3814f5e5f8c19c07ecaa679643f4
Kyle <berlinerkyle@gmail.com>
2026-02-20 11:15:01 +0100

    Initialized with base application file and respective testing file. These lack content so far.

---
+++ src/main/java/com/fts/flight_ticketing_system/FlightTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fts.flight_ticketing_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightTicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightTicketingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fts/flight_ticketing_system/FlightTicketingSystemApplicationTests.java
@@ -0,0 +1,18 @@
+package com.fts.flight_ticketing_system;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightTicketingSystemApplicationTests {
+
+	@Test
+	void shouldPass() {
+		assertTrue(true);
+	}
+
+}
