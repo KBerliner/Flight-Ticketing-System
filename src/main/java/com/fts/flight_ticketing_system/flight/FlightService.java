2be39d011670a80b398035c2be011707ea1ed4c1
Kyle <berlinerkyle@gmail.com>
2026-03-08 00:07:25 +0100

    Changed getting all flights to specifically return the hashmaps. Also added adding a flight to the DB.

--- src/main/java/com/fts/flight_ticketing_system/flight/FlightService.java
+++ src/main/java/com/fts/flight_ticketing_system/flight/FlightService.java
@@ -3,11 +3,13 @@ package com.fts.flight_ticketing_system.flight;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.zip.DataFormatException;
 
 import com.fts.flight_ticketing_system.FlightTicketingSystemApplication;
 import com.fts.flight_ticketing_system.database.Database;
 import com.fts.flight_ticketing_system.database.Table;
 import com.fts.flight_ticketing_system.database.Rows.Row;
+import com.fts.flight_ticketing_system.database.Rows.RowFactory.ROWTYPE;
 
 public class FlightService {
     private Database database = FlightTicketingSystemApplication.database;
@@ -17,18 +19,23 @@ public class FlightService {
         flightTable = database.createTable("flights");
     }
 
-    public List getAllFlights() {
+    public List<HashMap<String, Object>> getAllFlights() {
         Row[] rows = flightTable.getRows();
         List<HashMap<String, Object>> hashMapRows = new ArrayList<>();
-
+        
         for (Row row : rows) {
             Flight flight = (Flight) row.getContent();
 
-            HashMap<String, Object> jsonHashMap = flight.getflightAsHashMap();
+            HashMap<String, Object> jsonHashMap = flight.getFlightAsHashMap();
 
             hashMapRows.add(jsonHashMap);
         }
 
         return hashMapRows;
+    }
+
+    public void createFlight(Flight flight) throws DataFormatException {
+        // TODO: THIS SHOULD NOT JUST THROW THE EXCEPTION, THE EXCEPTION SHOULD BE HANDLED HERE
+        flightTable.insertEntry(ROWTYPE.FLIGHT, flight.getId(), flight);
     }
 }
--- src/test/java/com/fts/flight_ticketing_system/unit/flight/flightServiceTests.java
+++ src/test/java/com/fts/flight_ticketing_system/unit/flight/flightServiceTests.java
@@ -2,11 +2,16 @@ package com.fts.flight_ticketing_system.unit.flight;
 
 import static org.junit.Assert.assertEquals;
 
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.zip.DataFormatException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.fts.flight_ticketing_system.flight.Flight;
 import com.fts.flight_ticketing_system.flight.FlightService;
 
 public class flightServiceTests {
@@ -22,5 +27,19 @@ public class flightServiceTests {
         List flights = flightService.getAllFlights();
 
         assertEquals(0, flights.size());
+    }
+
+    @Test
+    void shouldAddFlightToDB() throws DataFormatException {
+        Flight flight = new Flight(10.0, ZonedDateTime.now(), Duration.ofHours(2));
+
+        flightService.createFlight(flight);
+
+        List<HashMap<String, Object>> flights = flightService.getAllFlights();
+
+        HashMap<String, Object> retrievedFlight = flights.get(0);
+
+        assertEquals(1, flights.size());
+        assertEquals(flight.getFlightAsHashMap(), retrievedFlight);
     }
 }
